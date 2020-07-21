c0721744e3998548ad73b04feb3ec6940e575302
jumzeey <umatonjayrex01@example.com>
2020-07-21 18:25:57 +0100

    basic checkbox functionality completed

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//triggers the checkbox selection, it needs  cb parameter which attaches itself to the "this" keyword
+function toggleCB(cb){
+    if (cb.dataset.checked == 0) {
+        cb.style.background = "#89d814";
+        cb.dataset.checked = 1;
+    } else {
+        cb.style.background = "#eee";
+        cb.dataset.checked = 0;
+    }
+   
+}
+// triggers the submit button
 function submitValues() {
     alert('checkbox 1= ' + cb1.dataset.checked + ',' + ' checkbox 2= ' + cb2.dataset.checked)
 }
