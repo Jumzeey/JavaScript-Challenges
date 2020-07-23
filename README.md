6e14acc8540c1f69ed3a9c97c30bdb0fee48f56a
jumzeey <umatonjayrex01@example.com>
2020-07-23 18:17:26 +0100

    completed

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# Contact form with Ajax and php
-A modern HTML5 contact form, that uses some new HTML5 features, combined with Ajax and Php.
-Everything happens without reloading the page or navigating to another document to pass the data. You can use this snippets code on any site that processes php, and you can modify it for any other purposes very easily.
+# Detect user browser with javascript.
+snipppets of codes to help detect  a uses browser
--- script.js
+++ script.js
@@ -1,27 +1,13 @@
-//function to store document.getElementById
-function _(id) {
-    return document.getElementById(id);
-};
-//actions done on data submission
-function submitForm(){
-    _("mybtn").disabled = true;
-    _("status").innerHTML = "Please wait...";
-    let formdata = new formData();
-    formdata.append('n', _('n').value);
-    formdata.append('e', _('e').value);
-    formdata.append('m', _('m').value);
-    //new http request object
-    let ajax = new XMLHttpRequest();
-    ajax.open('post', 'parser.php');
-    ajax.onreadystatechange = function () {
-        if (ajax.readyState == 4 && ajax.status == 200) {
-            if (ajax.responseText == "success") {
-                _('my_form').innerHTML = '<h2>Thanks ' + _('n').value + ', your message has been sent</h2>';
-            } else {
-                _('status').innerHTML = ajax.responseText;
-                _('mybtn').disabled = false;
-            }
-        }
+let bt = ['Chrome','Firefox','Safari','Opera','Edge','MSIE','Trident'];
+let b; userAgent = navigator.userAgent;
+for (let i = 0; i < bt.length; i++) {
+    if (userAgent.indexOf(bt[i]) != -1) {
+        b = bt[i];
+        break;
     }
-    ajax.send('formData');
+
 }
+if (b == 'MSIE' || b == 'Edge' || b == 'Trident') {
+    b = 'Internet Explorer';
+}
+alert(`You are using ${b} browser`);
