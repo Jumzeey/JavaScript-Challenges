1e1196620e25c535d085c4fedd084699bbcb1f10
jumzeey <umatonjayrex01@example.com>
2020-09-28 21:36:16 +0100

    complete

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-function editContent(div){
-    div.style.border = '1px solid #000';
-    div.style.padding = '20px';
-    div.contentEditable = true;
-};
-function uneditContent(div){
-    div.style.border = 'none';
-    div.style.padding = '0px';
-    div.contentEditable = false;
+function check(input){
+    var regex = /[^a-z]/gi;
+    input.value = input.value.replace(regex, "")
 }
-/* added features
-1. edittabel only for particular users
-2. after editting, submit to database
-3. have a edit button instead of cliucking directly
-*/
