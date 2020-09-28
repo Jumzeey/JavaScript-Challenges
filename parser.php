14529a4e80f2c362004a9fa21b2f472ee49be4b2
jumzeey <umatonjayrex01@example.com>
2020-09-28 21:09:30 +0100

    completed

--- parser.php
+++
@@ -1,5 +0,0 @@
-<?php
-    if (isset($_POST['n']) && isset($_POST['e']) && isset($_POST['m'])) {
-      
-    }
-?>
--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-//function to store document.getElementById
-function _(id) {
-    return document.getElementById(id);
+function editContent(div){
+    div.style.border = '1px solid #000';
+    div.style.padding = '20px';
+    div.contentEditable = true;
 };
-//actions done on data submission
-function submitForm(){
-    _("mybtn").disabled = true;
-    _("status").innerHTML = "Please wait...";
-    let formdata = new formData();
-    formdata.append('n', _('n').value);
-    formdata.append('e', _('e').value);
-    formdata.append('m', _('m').value);
-    //new http request object
-    let ajax = new XMLHttpRequest();
-    ajax.open('post', 'parser.php');
-    ajax.onreadystatechange = () => {
-        if (ajax.readyState == 4 && ajax.status == 200) {
-            if (ajax.responseText == "success") {
-                _('my_form').innerHTML = '<h2>Thanks ' + _('n').value + ', your message has been sent</h2>';
-            } else {
-                _('status').innerHTML = ajax.responseText;
-                _('mybtn').disabled = false;
-            }
-        }
-    }
-    ajax.send('formData');
+function uneditContent(div){
+    div.style.border = 'none';
+    div.style.padding = '0px';
+    div.contentEditable = false;
 }
+/* added features
+1. edittabel only for particular users
+2. after editting, submit to database
+3. have a edit button instead of cliucking directly
+*/
